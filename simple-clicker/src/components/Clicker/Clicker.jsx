951ac40788d8900edb064b0a7f26e90996285f40
Valeria Shevtsova <vsshevtsova@yandex.ru>
2024-11-12 11:42:30 +0300

    refactor: useState's props name; function emptyValue name, code reduce
    Исправлено название параметра пары useState; функция emptyValue переименована в defaultValue, тело функции сокращено с помощью тернарного оператора.

--- simple-clicker/src/components/Clicker/Clicker.jsx
+++ simple-clicker/src/components/Clicker/Clicker.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Clicker() {
-    const [clickCount, updateCount] = useState(0)
-    const [inputValue, getState] = useState('')
+  const [clickCount, updateCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
-    function emptyValue(value){
-        if(value === '') {
-            return 'Никто не пишет...';
-        }
-        return value;
-    }
+  function defaultValue(value) {
+    return value === "" ? "Никто не пишет..." : value;
+  }
 
-    return (
-        <div className='container'>
-            <div className = 'container__block'>
-                    <p className='block__title txt'> Кликов: {clickCount} </p>
-                <button onClick={() => updateCount(clickCount + 1)} className='block__btn txt'> Нажми меня! </button>
-            </div>
-            <div className = 'container__block'>
-                <p className='block__title txt'> {emptyValue(inputValue)} </p>
-                <input
-                    onChange={(event) => getState(event.target.value)} className='block__input txt' type="text" value={inputValue} placeholder='Напиши что-нибудь...' />
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="container__block">
+        <p className="block__title txt"> Кликов: {clickCount} </p>
+        <button
+          onClick={() => updateCount(clickCount + 1)}
+          className="block__btn txt"
+        >
+          Нажми меня!
+        </button>
+      </div>
+      <div className="container__block">
+        <p className="block__title txt"> {defaultValue(inputValue)} </p>
+        <input
+          onChange={(event) => setInputValue(event.target.value)}
+          className="block__input txt"
+          type="text"
+          value={inputValue}
+          placeholder="Напиши что-нибудь..."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Clicker;
